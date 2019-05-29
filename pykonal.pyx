45b0f921c16245962780ef080b23fd29faad2208
Malcolm White <malcolcw@usc.edu>
2019-05-29 15:29:50 -0400

    UPGRADE:: Implement 2nd-order update scheme

--- pykonal.pyx
+++ pykonal.pyx
@@ -5,19 +5,7 @@ cimport libc.math
 
 DTYPE = np.float32
 
-cdef float MAX_FLOAT = np.finfo(DTYPE).max 
-
-class EikonalSolver2D(object):
-    def __init__(self, vv, dx, dy):
-        self._vv = vv
-        self._dx, self._dy = dx, dy
-
-    def solve(self):
-        uu, is_alive, close, is_far = init_lists(self._vv)
-        init_source(uu, close, is_far)
-        update(uu, self._vv, is_alive, close, is_far, self._dx, self._dy)
-        self.uu =  uu
-
+cdef float MAX_FLOAT = np.finfo(DTYPE).max
 
 def init_lists(vv):
     uu       = np.full(vv.shape, fill_value=MAX_FLOAT, dtype=DTYPE)
@@ -36,8 +24,7 @@ def init_source(uu, close, is_far):
     return (uu, close, is_far)
 
 
-def pykonal(vv):
-    dx, dy = 1, 1
+def pykonal(vv, dx=1, dy=1):
     uu, is_alive, close, is_far = init_lists(vv)
     init_source(uu, close, is_far)
     update(uu, vv, is_alive, close, is_far, dx, dy)
@@ -74,99 +61,147 @@ def update(
     is_far - Array of bool values indicating whether a node has a temporary
              value.
     '''
-    cdef Py_ssize_t ix, iy
-    cdef Py_ssize_t trial_ix, trial_iy
-    cdef Py_ssize_t nbr_ix, nbr_iy
-    cdef Py_ssize_t max_ix, max_iy
-    cdef float u0
-    cdef float ux, ux1, ux2, uy, uy1, uy2
-    cdef float ddx2, ddy2
-    cdef float slo2, a, b, c
-    cdef float unew
-    cdef Py_ssize_t i
+    cdef Py_ssize_t       i, iax
     cdef Py_ssize_t[4][2] nbrs
+    cdef Py_ssize_t[2]    max_idx, nbr, trial_idx
+    cdef Py_ssize_t[2]    switch
+    cdef int              bord, ford, drxn
+    cdef float            a, b, c, bfd, ffd
+    cdef float[2]         aa, bb, cc, dd, dd2
 
-    max_ix, max_iy = is_alive.shape[0], is_alive.shape[1]
+    max_idx  = [is_alive.shape[0], is_alive.shape[1]]
+    dd       = [dx, dy]
+    dd2      = [dx**2, dy**2]
+    dx2, dy2 = dx**2, dy**2
+    for iax in range(2):
+        assert dd[iax] > 0
 
     while len(close) > 0:
         # Let Trial be the point in Close with the smallest value of u
         close.sort(key=lambda idx: uu[idx[0], idx[1]])
         trial_idx = heapq.heappop(close)
-        trial_ix, trial_iy = trial_idx[0], trial_idx[1]
-        is_alive[trial_ix, trial_iy] = True
+        is_alive[trial_idx[0], trial_idx[1]] = True
 
-        nbrs[0][0] = trial_ix-1
-        nbrs[0][1] = trial_iy
-        nbrs[1][0] = trial_ix+1
-        nbrs[1][1] = trial_iy
-        nbrs[2][0] = trial_ix
-        nbrs[2][1] = trial_iy-1
-        nbrs[3][0] = trial_ix
-        nbrs[3][1] = trial_iy+1
+        nbrs[0][0] = trial_idx[0] - 1
+        nbrs[0][1] = trial_idx[1]
+        nbrs[1][0] = trial_idx[0] + 1
+        nbrs[1][1] = trial_idx[1]
+        nbrs[2][0] = trial_idx[0]
+        nbrs[2][1] = trial_idx[1] - 1
+        nbrs[3][0] = trial_idx[0]
+        nbrs[3][1] = trial_idx[1] + 1
         for i in range(4):
             nbr_ix = nbrs[i][0]
             nbr_iy = nbrs[i][1]
-            if not stencil(nbr_ix, nbr_iy, max_ix, max_iy) \
-                    or is_alive[nbr_ix, nbr_iy]:
+            nbr    = nbrs[i]
+            if not stencil(nbr[0], nbr[1], max_idx[0], max_idx[1]) \
+                    or is_alive[nbr[0], nbr[1]]:
                 continue
-            u0 = uu[nbr_ix, nbr_iy]
-            # Recompute the values of u at all Close neighbours of Trial by solving
-            # the piecewise quadratic equation.
-            if nbr_ix > 0:
-                ux1 = uu[nbr_ix-1, nbr_iy]
-            else:
-                ux1 = MAX_FLOAT
-            if nbr_ix < uu.shape[0] - 1:
-                ux2 = uu[nbr_ix+1, nbr_iy]
-            else:
-                ux2 = MAX_FLOAT
-            if ux1 < ux2:
-                ux = ux1
-            else:
-                ux = ux2
-
-            if nbr_iy > 0:
-                uy1 = uu[nbr_ix, nbr_iy-1]
-            else:
-                uy1 = MAX_FLOAT
-            if nbr_iy < uu.shape[1] - 1:
-                uy2 = uu[nbr_ix, nbr_iy+1]
-            else:
-                uy2 = MAX_FLOAT
-            if uy1 < uy2:
-                uy = uy1
-            else:
-                uy = uy2
-
-            if ux < u0:
-                ddx2 = (1/dx)**2
-            else:
-                ux, ddx2 = 0, 0
-            if uy < u0:
-                ddy2 = (1/dy)**2
-            else:
-                uy, ddy2 = 0, 0
-
-            slo2 = 1/vv[nbr_ix, nbr_iy]**2
-
-            a = ddx2 + ddy2
+            # Recompute the values of u at all Close neighbours of Trial
+            # by solving the piecewise quadratic equation.
+            for iax in range(2):
+                switch = [0, 0]
+                switch[iax] = 1
+                if nbr[iax] > 1 \
+                        and not is_far[nbr[0]-switch[0]*2, nbr[1]-switch[1]*2] \
+                        and not is_far[nbr[0]-switch[0], nbr[1]-switch[1]]:
+                    bord = 2
+                    bfd  = (
+                        3 * uu[nbr[0],             nbr[1]] \
+                      - 4 * uu[nbr[0]-switch[0],   nbr[1]-switch[1]] \
+                      +     uu[nbr[0]-switch[0]*2, nbr[1]-switch[1]*2]
+                    ) / (2 * dd[iax])
+                elif nbr[iax] > 0 \
+                        and not is_far[nbr[0]-switch[0], nbr[1]-switch[1]]:
+                    bord = 1
+                    bfd  = (
+                        uu[nbr[0],           nbr[1]]
+                      - uu[nbr[0]-switch[0], nbr[1]-switch[1]]
+                    ) / dd[iax]
+                else:
+                    bfd, bord = 0, 0
+                if nbr[iax] < max_idx[iax] - 2 \
+                        and not is_far[nbr[0]+switch[0]*2, nbr[1]+switch[1]*2] \
+                        and not is_far[nbr[0]+switch[0], nbr[1]+switch[1]]:
+                    ford = 2
+                    ffd  = (
+                        3 * uu[nbr[0],             nbr[1]] \
+                      - 4 * uu[nbr[0]+switch[0],   nbr[1]+switch[1]] \
+                      +     uu[nbr[0]+switch[0]*2, nbr[1]+switch[1]*2]
+                    ) / (2 * dd[iax])
+                elif nbr[iax] < max_idx[iax]-1 \
+                        and not is_far[nbr[0]+switch[0], nbr[1]+switch[1]]:
+                    ford = 1
+                    ffd  = (
+                        uu[nbr[0]+switch[0], nbr[1]+switch[1]]
+                      - uu[nbr[0],           nbr[1]]
+                    ) / dd[iax]
+                else:
+                    ffd, ford = 0, 0
+                if bfd > -ffd:
+                    order, drxn = bord, -1
+                else:
+                    order, drxn = ford, 1
+                if order == 2:
+                    aa[iax] = 9 / (4 * dd2[iax])
+                    bb[iax] = (
+                        6*uu[nbr[0]+2*drxn*switch[0], nbr[1]+2*drxn*switch[1]]
+                     - 24*uu[nbr[0]+drxn*switch[0], nbr[1]+drxn*switch[1]]
+                    ) / (4 * dd2[iax])
+                    cc[iax] = (
+                        uu[
+                            nbr[0]+2*drxn*switch[0],
+                            nbr[1]+2*drxn*switch[1]
+                        ]**2 \
+                        - 8 * uu[
+                            nbr[0]+2*drxn*switch[0],
+                            nbr[1]+2*drxn*switch[1]
+                        ] * uu[
+                            nbr[0]+drxn*switch[0],
+                            nbr[1]+drxn*switch[1]
+                        ]
+                        + 16 * uu[
+                            nbr[0]+drxn*switch[0],
+                            nbr[1]+drxn*switch[1]
+                        ]**2
+                    ) / (4 * dd2[iax])
+                elif order == 1:
+                    aa[iax] = 1 / dd2[iax]
+                    bb[iax] = -2 * uu[
+                        nbr[0]+drxn*switch[0],
+                        nbr[1]+drxn*switch[1]
+                    ] / dd2[iax]
+                    cc[iax] = uu[
+                        nbr[0]+drxn*switch[0],
+                        nbr[1]+drxn*switch[1]
+                    ]**2 / dd2[iax]
+                elif order == 0:
+                    aa[iax], bb[iax], cc[iax] = 0, 0, 0
+                else:
+                    raise (Exception('Huh!?'))
+            a = aa[0] + aa[1]
+            b = bb[0] + bb[1]
+            c = cc[0] + cc[1] - 1/vv[nbr[0], nbr[1]]**2
             if a == 0:
-                print(f'WARNING :: a == 0 {nbr_ix}, {nbr_iy}')
+                print(f'WARNING(2) :: a == 0 {nbr[0]}, {nbr[1]}')
                 continue
-            b = -2 * (ddx2 * ux + ddy2 * uy)
-            c = (ddx2 * (ux ** 2) + ddy2 * (uy ** 2) - slo2)
             if b ** 2 < 4 * a * c:
-                unew = -b / (2 * a) # This may not be mathematically permissible...
-                print(f'WARNING :: determinant is negative {nbr_ix}, {nbr_iy}: {100*np.sqrt(4 * a * c - b**2)/(2*a)/unew}')
+                # This may not be mathematically permissible
+                uu[nbr[0], nbr[1]] = -b / (2 * a)
+                print(
+                    f'WARNING(2) :: determinant is negative {nbr[0]}, {nbr[1]}:'
+                    f'{100*np.sqrt(4 * a * c - b**2)/(2*a)/uu[nbr[0], nbr[1]]}'
+                )
             else:
-                unew = (-b + libc.math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-            uu[nbr_ix, nbr_iy] = unew
+                uu[nbr[0], nbr[1]] = (
+                    -b + libc.math.sqrt(b ** 2 - 4 * a * c)
+                ) / (2 * a)
             # Tag as Close all neighbours of Trial that are not Alive
             # If the neighbour is in Far, remove it from that list and add it to
             # Close
-            if is_far[nbr_ix, nbr_iy]:
-                heapq.heappush(close, [nbr_ix, nbr_iy])
-                is_far[nbr_ix, nbr_iy] = False
+            if is_far[nbr[0], nbr[1]]:
+                heapq.heappush(close, [nbr[0], nbr[1]])
+                is_far[nbr[0], nbr[1]] = False
 
     return
 
