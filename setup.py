c579107f3be7b2d802b7424b652f794229ebf126
Malcolm White <malcolm.white@usc.edu>
2019-06-13 11:38:06 -0400

    UPDATE:: Add detail to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,70 @@
-from distutils.core import setup
+'''
+setup.py adapted from https://github.com/kennethreitz/setup.py
+'''
+import io
 import numpy as np
+import os
+from distutils.core import setup
 from Cython.Build import cythonize
 
+#import sys
+#from shutil import rmtree
+#from setuptools import find_packages, setup, Command
+
+
+# Package meta-data.
+name            = 'pykonal'
+description     = 'Solver for the Eikonal equation in 3D Cartesian coordiantes.'
+url             = 'https://github.com/malcolmw/pykonal'
+email           = 'malcolm.white@.usc.edu'
+author          = 'Malcolm C. A. White'
+requires_python = '>=3'
+required        = ['cython', 'numpy', 'scipy']
+ext_modules     = cythonize('pykonal/pykonal.pyx')
+include_dirs    = [np.get_include()]
+license         = 'GNU GPLv3'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = description
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+project_slug = name.lower().replace("-", "_").replace(" ", "_")
+with open(os.path.join(here, project_slug, '__version__.py')) as f:
+    exec(f.read(), about)
+
+# Where the magic happens:
 setup(
-    name='pykonal',
-    ext_modules=cythonize("pykonal*.pyx", annotate=True),
-    include_dirs=[np.get_include()]
+    name=name,
+    version=about['__version__'],
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=author,
+    author_email=email,
+    python_requires=requires_python,
+    url=url,
+    ext_modules=ext_modules,
+    include_dirs=include_dirs,
+    install_requires=required,
+    license=license,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Scientific/Engineering :: Physics'
+    ]
 )
