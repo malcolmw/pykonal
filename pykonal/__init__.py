304f33db8bf375c507dce922650204599713f676
Malcolm White <malcolcw@usc.edu>
2020-04-25 15:17:38 -0400

    Make version data available through root package.

--- pykonal/__init__.py
+++ pykonal/__init__.py
@@ -1,3 +1,13 @@
+from .__version__ import (
+    __major_version__,
+    __minor_version__,
+    __minor_subversion__,
+    __release__,
+    __patch__,
+    __version_number__,
+    __version__
+)
+
 from .solver import EikonalSolver
 
 from . import constants
--- pykonal/__version__.py
+++ pykonal/__version__.py
@@ -1,1 +1,13 @@
-__version__ = '0.2.3a0'
+__major_version__    = 0
+__minor_version__    = 2
+__minor_subversion__ = 3
+__release__          = "a"
+__patch__            = 0
+__version_number__   = ".".join(
+    (
+        str(__major_version__),
+        str(__minor_version__),
+        str(__minor_subversion__)
+    )
+)
+__version__          = f"{__version_number__}{__release__}{__patch__}"
