276fa35eeb145278fdd01f543be1c0b38ab24ab4
Malcolm White <malcolmw@mit.edu>
2023-04-26 09:47:40 -0400

    Fix incorrect versioning in __version__.py

--- pykonal/__version__.py
+++ pykonal/__version__.py
@@ -1,11 +1,11 @@
 __major_version__  = 0
-__minor_version__  = 3
-__patch__          = 2
-__release__        = "b5"
+__minor_version__  = 2
+__patch__          = 3
+__release__        = 'b5'
 __version_tuple__ = (
     __major_version__,
     __minor_version__,
     __patch__
 )
-__version_number__ = ".".join([str(v) for v in __version_tuple__])
-__version__ = f"{__version_number__}{__release__}"
+__version_number__ = '.'.join([str(v) for v in __version_tuple__])
+__version__ = f'{__version_number__}{__release__}'
